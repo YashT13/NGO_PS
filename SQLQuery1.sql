73edae32fb1459e0b62310c991fa86698888e27e
Sabyasachi Ghosh <34605@ITCINFOTECH.com>
2019-08-29 14:43:20 +0530

    initial commit

---
+++ SQLQuery1.sql
@@ -0,0 +1,13 @@
+create table LoginInfo (Username varchar(30) primary key, [Password] varchar(30))
+select * from Basketball
+alter table Student drop column Attendance
+select * from Chess
+
+
+create Proc SP_InsertReg
+{ 
+	StudentName varchar(20)
+}
+As 
+Begin
+	insert into
